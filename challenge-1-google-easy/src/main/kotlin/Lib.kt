32f15b650444bf9c5805007ac1e28d575d6dc4df
Hernand Azevedo <hernand.azevedo@gmail.com>
2021-07-24 21:29:27 -0300

    :wrench: Improve code with only one iterations
    Signed-off-by: Hernand Azevedo <hernand.azevedo@gmail.com>

--- challenge-1-google-easy/src/main/kotlin/Lib.kt
+++ challenge-1-google-easy/src/main/kotlin/Lib.kt
@@ -7,16 +7,23 @@ object Lib {
      */
     fun canAdd(array: Array<Int>, k: Int): Boolean {
 
-        var i = 0
-        while (i <= array.size - 1) {
-            var j = i + 1
-            while (j <= array.size - 1) {
-                if (checkIfSumsIsEqualTo(array, i, j, k)) {
-                    return true
-                }
-                j++
+        var leftOperandIndex = 0
+        var rightOperandIndex = leftOperandIndex + 1
+        var arraySize = array.size
+        var lastLeftOperandIndex = arraySize - 2
+        var lastRightOperandIndex = arraySize - 1
+
+        while (leftOperandIndex <= lastLeftOperandIndex && rightOperandIndex <= lastRightOperandIndex) {
+            if (checkIfSumsIsEqualTo(array, leftOperandIndex, rightOperandIndex, k)) {
+                return true
             }
-            i++
+
+            if(rightOperandIndex == lastRightOperandIndex) {
+                leftOperandIndex++
+                rightOperandIndex = leftOperandIndex + 1
+            } else {
+                rightOperandIndex++
+            }
         }
 
         return false
@@ -24,9 +31,9 @@ object Lib {
 
     private fun checkIfSumsIsEqualTo(
         array: Array<Int>,
-        i: Int,
-        j: Int,
+        leftOperandIndex: Int,
+        rightOperandIndex: Int,
         k: Int
-    ) = array[i] + array[j] == k
+    ) = array[leftOperandIndex] + array[rightOperandIndex] == k
 }
 
